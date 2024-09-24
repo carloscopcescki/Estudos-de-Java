577e24899c0666cb325ae91878fac15bd03f0598
Copcescki <carlos.copcescki@gmail.com>
2024-09-23 22:40:32 -0300

    Acessing static method in main.java Ex 2

--- Ex 2 - Pacotes/src/com/br/fsa/Main.java
+++ Ex 2 - Pacotes/src/com/br/fsa/Main.java
@@ -6,5 +6,12 @@ public class Main {
         p1.altura = 180;
         System.out.println(p1.calculaImc());
         System.out.println(p1.dizONome());
+        p1.ehIMCOk(p1);
+
+        Pessoa p2 = new Pessoa(80, 150);
+        p2.altura = 150;
+        p2.peso = 160;
+        System.out.println(p2.calculaImc());
+        p2.ehIMCOk(p2);
     }
 }
